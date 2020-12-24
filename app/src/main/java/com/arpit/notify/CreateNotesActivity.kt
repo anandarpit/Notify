64f2ee82f3dffc4a6de989a0a0d153484886cfc2
Arpit Anand <orionarpit@gmail.com>
2020-12-24 21:48:12 +0530

    add notes layout complete

---
+++ app/src/main/java/com/arpit/notify/CreateNotesActivity.kt
@@ -0,0 +1,11 @@
+package com.arpit.notify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_notes)
+    }
+}
--- app/src/main/java/com/arpit/notify/MainActivity.kt
+++ app/src/main/java/com/arpit/notify/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.arpit.notify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add_note.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,CreateNotesActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
