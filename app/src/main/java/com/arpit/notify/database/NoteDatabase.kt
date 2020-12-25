8c6fd2e51218beea18404542b12be601f40ef4dd
Arpit Anand <orionarpit@gmail.com>
2020-12-25 20:23:03 +0530

    room database complete

--- app/src/main/java/com/arpit/notify/database/NoteDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.arpit.notify.database
-
-import androidx.room.Database
-import com.arpit.notify.entities.Note
-
-
-@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false )
-class NoteDatabase {
-}
---
+++ app/src/main/java/com/arpit/notify/database/NotesDatabase.java
@@ -0,0 +1,30 @@
+package com.arpit.notify.database;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.arpit.notify.dao.NoteDao;
+import com.arpit.notify.entities.Note;
+
+@Database(entities = Note.class, version = 1, exportSchema = false)
+abstract class NotesDatabase extends RoomDatabase {
+
+    private static NotesDatabase notesDatabase;
+
+    public static synchronized NotesDatabase getDatabase(Context context)
+    {
+        if(notesDatabase == null){
+            Room.databaseBuilder(context
+            , NotesDatabase.class,
+                    "note_db"
+                    ).build();
+        }
+        return notesDatabase;
+    }
+
+    public abstract NoteDao noteDao(); // needs to have abstract method related to each dao that you have created here only 1 I have created
+}
